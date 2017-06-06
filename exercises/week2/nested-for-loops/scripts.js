e865072523b6bc6f94d7b7f179eb0a5e4797744e
danyonsatterlee <danyon.satterlee@gmail.com>
2017-06-06 16:43:51 -0600

    nested for loop practice

---
+++ exercises/week2/nested-for-loops/scripts.js
@@ -0,0 +1,16 @@
+// Using a for loop within a for loop create an array that looks like this when printed to the console:
+//
+// ["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// Here's the array of names and the alphabet to get you started:
+//
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var result = [];
+
+for (var i=0; i<people.length; i++){
+  result.push(people[i] + ":");
+  for (var x=0; x<alphabet.length; x++){
+    result.push(alphabet[x]);
+  }
+}
+console.log(result);

935682bcb54bbb461cac1d3df5e8a7895e1a5e33
danyonsatterlee <danyon.satterlee@gmail.com>
2017-06-07 15:33:53 -0600

    update colors

--- exercises/week2/dj-box/scripts.js
+++ exercises/week2/dj-box/scripts.js
@@ -17,7 +17,45 @@ for (var i=0; i<colorBox.length; i++) {
 var orange = document.getElementById('biggestBox');
 window.addEventListener('scroll', function(){
   orange.style.backgroundColor="rgb(215, 145, 24)";
-  });
+});
 
-var keyColor
-//
+
+
+  // window.addEventListener("keypress", function(event){
+  //   if (event.which===82){
+  //     document.getElementById('biggestBox').style.backgroundColor="red";
+  //   } else if (event.which===82){
+  //   document.getElementById('biggestBox').style.backgroundColor="green";
+  //   }
+  //   else if (event.which===82){
+  //       document.getElementById('biggestBox').style.backgroundColor="blue";
+  //   } else if (event.which===82){
+  //       document.getElementById('biggestBox').style.backgroundColor="green";
+  //   }
+  //
+  //
+  // });
+
+
+
+
+
+
+
+
+  // function randomKey(){
+  //   var keyBox= document.getElementsByClassName('lilBox');
+  //
+  //   for (var i=0; i<keyBox.length; i++)
+  //   var randomize = Math.random(keyBox[i]);
+  //   console.log(randomize);
+  //   if (82===e.keycode){
+  //     randomize.style.backgroundColor="red";
+  //   } else if (71===e.keycode){
+  //     randomize.style.backgroundColor="green";
+  //   }
+  //   else if (66===e.keycode){
+  //     randomize.style.backgroundColor="blue";
+  //   } else if (89===e.keycode){
+  //     randomize.style.backgroundColor="green";
+  //   }

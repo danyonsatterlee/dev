c1e6213acf0620281a82788fdb5a6395a0b586df
danyonsatterlee <danyon.satterlee@gmail.com>
2017-06-07 13:38:55 -0600

    calculator working

---
+++ exercises/week2/calculator/scripts.js
@@ -0,0 +1,29 @@
+document.getElementById("addTogether").addEventListener("click", function() {
+  var result = Number(document.getElementById("add1").value) + Number(document.getElementById("add2").value);
+  document.getElementById("resultAdd").innerHTML = result
+
+});
+
+document.getElementById("subTogether").addEventListener("click", function() {
+  var result = Number(document.getElementById("sub1").value) - Number(document.getElementById("sub2").value);
+  document.getElementById("resultSub").innerHTML = result
+
+});
+
+document.getElementById("mulTogether").addEventListener("click", function() {
+  var result = Number(document.getElementById("mul1").value) * Number(document.getElementById("mul2").value);
+  document.getElementById("resultMul").innerHTML = result
+
+});
+
+document.getElementById("divideTogether").addEventListener("click", function() {
+  var result = Number(document.getElementById("div1").value) / Number(document.getElementById("div2").value);
+  document.getElementById("resultDiv").innerHTML = result
+
+});
+
+document.getElementById("moduleTogether").addEventListener("click", function() {
+  var result = Number(document.getElementById("mod1").value) % Number(document.getElementById("mod2").value);
+  document.getElementById("resultMod").innerHTML = result
+
+});

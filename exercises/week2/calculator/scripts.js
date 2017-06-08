9eb35ab57be52a145145b096ba6895d2dd683f88
danyonsatterlee <danyon.satterlee@gmail.com>
2017-06-07 21:33:13 -0600

    css to calculator and one input and result

--- exercises/week2/calculator/scripts.js
+++ exercises/week2/calculator/scripts.js
@@ -5,25 +5,65 @@ document.getElementById("addTogether").addEventListener("click", function() {
 });
 
 document.getElementById("subTogether").addEventListener("click", function() {
-  var result = Number(document.getElementById("sub1").value) - Number(document.getElementById("sub2").value);
+  var result = Number(document.getElementById("add1").value) - Number(document.getElementById("add2").value);
+
   document.getElementById("result").innerHTML = result
 
 });
 
 document.getElementById("mulTogether").addEventListener("click", function() {
-  var result = Number(document.getElementById("mul1").value) * Number(document.getElementById("mul2").value);
+  var result = Number(document.getElementById("add1").value) * Number(document.getElementById("add2").value);
+
   document.getElementById("result").innerHTML = result
 
 });
 
 document.getElementById("divideTogether").addEventListener("click", function() {
-  var result = Number(document.getElementById("div1").value) / Number(document.getElementById("div2").value);
+  var result = Number(document.getElementById("add1").value) / Number(document.getElementById("add2").value);
+
   document.getElementById("result").innerHTML = result
 
 });
 
 document.getElementById("moduleTogether").addEventListener("click", function() {
-  var result = Number(document.getElementById("mod1").value) % Number(document.getElementById("mod2").value);
+  var result = Number(document.getElementById("add1").value) % Number(document.getElementById("add2").value);
   document.getElementById("result").innerHTML = result
 
 });
+
+
+
+
+
+
+//////////
+
+// document.getElementById("addTogether").addEventListener("click", function() {
+//   var result = Number(document.getElementById("add1").value) + Number(document.getElementById("add2").value);
+//   document.getElementById("result").innerHTML = result
+//
+// });
+//
+// document.getElementById("subTogether").addEventListener("click", function() {
+//   var result = Number(document.getElementById("sub1").value) - Number(document.getElementById("sub2").value);
+//   document.getElementById("result").innerHTML = result
+//
+// });
+//
+// document.getElementById("mulTogether").addEventListener("click", function() {
+//   var result = Number(document.getElementById("mul1").value) * Number(document.getElementById("mul2").value);
+//   document.getElementById("result").innerHTML = result
+//
+// });
+//
+// document.getElementById("divideTogether").addEventListener("click", function() {
+//   var result = Number(document.getElementById("div1").value) / Number(document.getElementById("div2").value);
+//   document.getElementById("result").innerHTML = result
+//
+// });
+//
+// document.getElementById("moduleTogether").addEventListener("click", function() {
+//   var result = Number(document.getElementById("mod1").value) % Number(document.getElementById("mod2").value);
+//   document.getElementById("result").innerHTML = result
+//
+// });

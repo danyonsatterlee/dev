15efdf7e1eec86396578cdef315758ef9830f155
danyonsatterlee <danyon.satterlee@gmail.com>
2017-06-10 21:05:44 -0600

    adding  for tools of pests and money working, not grand total yet

--- projects/mario-pest-control/scripts.js
+++ projects/mario-pest-control/scripts.js
@@ -1,95 +1,60 @@
-// click button to add to goomba
-//click button so goomba adds to grand total
-
-//click button so bob's add to bob's total
-// click button so bob's adds to grand total
-
-
-//click button so cheeps adds to total
-// click button so cheeps adds to grand total
-
-//click button to reset everything
-
-//  window.onload= function(e){
-//   document.getElementById("goomba-total").innerHTML = 0;
-//   document.getElementById("bob-total").innerHTML = 0;
-//   document.getElementById("cheep-total").innerHTML = 0;
-//   document.getElementById("goomba-price-total").innerHTML = 0;
-//   document.getElementById("bob-price-total").innerHTML = 0 ;
-//   document.getElementById("cheep-price-total").innerHTML = 0;
-// }
-
-var grandTotal = 0;
-var goomPrice = 5;
-var bobPrice = 7;
-var cheepPrice = 11;
 
 
 
+var goombaCoins = 5;
 
-//adds to goomba total and grand total
-var grandTotal = `${charge1}` + `${charge2}` + `${charge3}`;
-document.getElementById("goomba-go").addEventListener("click", function() {
-  var goomba = document.getElementById("goomba-total").innerHTML = goombaTotal++;
-  var charge1 = goomba * goomPrice;
-  document.getElementById("goomba-price-total").innerHTML = charge1;
-  var grandTotal = `${charge1}` + `${charge2}` + `${charge3}`;
-  document.getElementById("grand-total").innerHTML = grandTotal;
+var goombaTotal = 0;
+var bobCoins = 7;
+var cheepCoins = 11;
+var bobTotal=0;
+var cheepTotal = 0;
+var moneyGoomba = goombaTotal * goombaCoins;
 
+function goomba() {
+  document.getElementById('goomba-go').addEventListener("click", function() {
+    goombaTotal++
+
+    document.getElementById('goomba-total').innerHTML = goombaTotal;
+    var moneyGoomba = goombaTotal * goombaCoins;
+    document.getElementById('goomba-price-total').innerHTML = moneyGoomba;
+})
+    document.getElementById('bob-go').addEventListener("click", function() {
+      bobTotal++
+      document.getElementById('bob-total').innerHTML = bobTotal;
+      var moneyBob = bobTotal * bobCoins;
+      document.getElementById('bob-price-total').innerHTML = moneyBob;
+
+  })
+  document.getElementById('cheep-go').addEventListener("click", function() {
+    cheepTotal++
+    document.getElementById('cheep-total').innerHTML = cheepTotal;
+    var moneyCheep = cheepTotal * cheepCoins;
+    document.getElementById('cheep-price-total').innerHTML =
+    (moneyCheep);
+
+  })
+  // document.getElementById('grand-total-go').innerHTML= moneyGoomba;
+
+
+
+
+
+}
+
+// var total1 = document.getElementbyId("goomba-price-total").innerHTML = goombaNum* goombaCoins;
+goomba();
+
+
+
+
+
+
+document.getElementById("reset").addEventListener("click", function() {
+  document.getElementById("cheep-total").innerHTML = (cheepTotal = 0);
+  document.getElementById("cheep-price-total").innerHTML = (moneyCheep=0);
+  document.getElementById("bob-total").innerHTML = (bobTotal = 0);
+  document.getElementById("bob-price-total").innerHTML = (moneyBob= 0);
+  document.getElementById("goomba-total").innerHTML = (goombaTotal = 0);
+    document.getElementById("goomba-price-total").innerHTML = (moneyBob = 0);
+  document.getElementById("grand-total").innerHTML = (grandTotal = 0);
 });
-
-
-
-document.getElementById("bob-go").addEventListener("click", function() {
-  var bob = document.getElementById("bob-total").innerHTML = goombaTotal++;
-  var charge2 = bob * bobPrice;
-  document.getElementById("bob-price-total").innerHTML = charge2;
-  var grandTotal = `${charge1}` + `${charge2}` + `${charge3}`;
-  document.getElementById("grand-total").innerHTML = grandTotal;
-
-});
-
-
-document.getElementById("cheep-go").addEventListener("click", function() {
-  var cheep = document.getElementById("cheep-total").innerHTML = goombaTotal++;
-  var charge3 = cheep * cheepPrice;
-  document.getElementById("cheep-price-total").innerHTML = charge3;
-  var grandTotal = `${charge1}` + `${charge2}` + `${charge3}`;
-  document.getElementById("grand-total").innerHTML = grandTotal;
-});
-
-
-//
-
-//
-//
-// var grand = 0;
-// var goom = 5;
-// var bob = 7;
-// var cheep =
-//
-//
-//
-// //adds to bob total and grand total
-// document.getElementById("bob-go").addEventListener("click", function() {
-//   var bob = document.getElementById("bob-total").innerHTML = (bobTotal++) * 7;
-//   var grand2 = document.getElementById("grand-total").innerHTML = grandTotal + bob;
-// });
-// //adds to cheep cheep total and grand total
-// document.getElementById("cheep-go").addEventListener("click", function() {
-//   var cheep = document.getElementById("cheep-total").innerHTML = (cheepTotal++) * 11;
-//   document.getElementById("grand-total").innerHTML = grandTotal + cheep;
-//
-// });
-//
-//
-// //suppose to reset goomba, bob cheep cheep and grand total to zero, but it  sets it to zero, but doesn't reset the counters
-// document.getElementById("reset").addEventListener("click", function() {
-//
-//
-//
-//   document.getElementById("cheep-total").innerHTML = (cheepTotal = 0);
-//   document.getElementById("bob-total").innerHTML = (bobTotal = 0);
-//   document.getElementById("goomba-total").innerHTML = (goombaTotal = 0);
-//   document.getElementById("grand-total").innerHTML = (grandTotal = 0);
-// });

7267db9875089cc47c33caecb976a0ac7334a730
danyonsatterlee <danyon.satterlee@gmail.com>
2017-08-02 11:41:39 -0600

    exersize

--- exercises/week10/todo-mongoose/index.js
+++ exercises/week10/todo-mongoose/index.js
@@ -1,90 +1,26 @@
+//should be in both my modules and index
 let mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-//27017 is the default database monogdb runs on
+const express = require("express");
+let bodyParser = require("body-parser");
+let cors = require("cors");
+
+
+let  toDoRouter = require("./routes/todo-route.js");
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+//use our routes 
+app.use("/todo",toDoRouter)
+
+app.listen(port, ()=>{
+    console.log(`listening to ${port}`)
+});
+
+//27017 is the default database monogdb runs on. needs to be in our index.js
 mongoose.connect("mongodb://localhost:27017/todos");
-let todoSchema = new Schema({
-    title:{
-        type: String,
-        required:true
-
-    },
-    description: String,
-    dateDue:{
-        type: String,
-        date:Date.now
-    },
-    price: Number
-});
-//monoose.model("collection name", schema)
-let Todo = mongoose.model("todolist", todoSchema);
-
-//tomorrow's date
-// let tomorrow= new Date();
-// tomorrow.setDate(tomorrow.getDate()+1);
-
-//create
-// let newTodo = new Todo({
-// title:"Connect mongoose to database",
-// description: "listen to Jacob",
-
-// });
-// save the todo to the mongodb database
-// newTodo.save((err,data)=>{
-//     if(err){
-//         console.log("err");
-//     } else {
-//         console.log("Item was saved to database");
-//         console.log(data);
-//     }
-
-// });
-// this is read all data the object and a callbackfunction
-// Todo.find({},(err,data)=>{
-//     if(err){
-//         console.log(err);
-//     } else{
-//         console.log("Found the data");
-//         console.log(data);
-//     }
-// });
-
-//read one data
-// Todo.findOne({_id:"5980bab6c9aa2442ac0df322"},(err,data)=>{
-//     if(err){
-//         console.log(err);
-//     } else{
-//         console.log("Found the data");
-//         console.log(data);
-//     }
-// });
-
-// Todo.findByIdAndRemove("5980bab6c9aa2442ac0df322", (err,data)=>{
-//     if(err){
-//         console.log(err);
-//     } else{
-//         console.log("Found the data");
-//         console.log(data);
-//     }
-// });
-
-let newData={
-    title:"again222333",
-    description:"updated yo222333"
-}
-Todo.findByIdAndUpdate("5980b5e1b1865d41b369dac0", newData, {new:true}, (err,data)=>{
-    if(err){
-        console.log(err);
-    } else{
-        console.log("update successful");
-        console.log(data);
-    }
-});
-
-Todo.find({},(err,data)=>{
-    if(err){
-        console.log(err);
-    } else{
-        console.log("Found the data");
-        console.log(data);
-    }
-});
